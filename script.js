1d27e8b6f398f31c45bc1d91161a3a403aa58f31
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-11-20 22:29:01 +0000

    Added dial for degrees

--- script.js
+++ script.js
@@ -15,12 +15,17 @@ document.onclick = (event) => {
   const classes = ['block', 'fade-in'];
   block.classList.add(...classes);
   $('.container').appendChild(block);
-  block.style.transform = getTransform(event);
+  const rotate = getTransform(event).rotate;
+  const scale = getTransform(event).scale;
+  block.style.transform = `${rotate} ${scale}`;
 }
 
 document[moveEvent] = (event) => {
+  const rotate = getTransform(event).rotate;
+  const scale = getTransform(event).scale;
+  $('.dial-arm').style.transform = rotate;
   $$('.block').forEach(item => {
-    item.style.transform = getTransform(event);
+    item.style.transform = `${rotate} ${scale}`;
   });
 }
 
@@ -28,7 +33,10 @@ const getTransform = (event) => {
   const evt = touchEnabled() ? event.touches[0] : event;
   const ratioX = window.innerWidth / 360;
   const ratioY = window.innerWidth / 3;
-  const degrees = Math.round(evt.clientX / ratioX);
+  const degrees = Math.ceil(evt.clientX / ratioX);
   const size = 0.1 + evt.clientY / ratioY;
-  return `rotate(${degrees}deg) scale(${size})`
+  return {
+    rotate: `rotate(${degrees}deg)`,
+    scale: `scale(${size})`
+  }  
 }
