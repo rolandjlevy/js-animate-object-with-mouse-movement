c11331dda375861444ecbbb79b88b4ac2d985a22
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-11-20 22:58:49 +0000

    Added an offset to each block

--- script.js
+++ script.js
@@ -12,28 +12,29 @@ let counter = 1;
 document.onclick = (event) => {
   if (counter++ >= 100) return;
   const block = document.createElement('div');
+  block.setAttribute('data-counter', counter);
   const classes = ['block', 'fade-in'];
   block.classList.add(...classes);
   $('.container').appendChild(block);
-  const rotate = getTransform(event).rotate;
+  const rotate = getTransform(event, counter).rotate;
   const scale = getTransform(event).scale;
   block.style.transform = `${rotate} ${scale}`;
 }
 
 document[moveEvent] = (event) => {
-  const rotate = getTransform(event).rotate;
-  const scale = getTransform(event).scale;
-  $('.dial-arm').style.transform = rotate;
-  $$('.block').forEach(item => {
+  $('.dial-arm').style.transform = getTransform(event, 0).rotate;
+  $$('.block').forEach((item) => {
+    const rotate = getTransform(event, item.dataset.counter).rotate;
+    const scale = getTransform(event).scale;
     item.style.transform = `${rotate} ${scale}`;
   });
 }
 
-const getTransform = (event) => {
+const getTransform = (event, index) => {
   const evt = touchEnabled() ? event.touches[0] : event;
   const ratioX = window.innerWidth / 360;
-  const ratioY = window.innerWidth / 3;
-  const degrees = Math.ceil(evt.clientX / ratioX);
+  const ratioY = window.innerWidth / 5;
+  const degrees = Math.ceil((evt.clientX) / ratioX) + Number(index) * 2;
   const size = 0.1 + evt.clientY / ratioY;
   return {
     rotate: `rotate(${degrees}deg)`,
